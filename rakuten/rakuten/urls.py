90dc91053651ad054197afd0eae46e5903b12954
takurinton <takuirnton@gmail.com>
2020-09-23 13:49:51 +0900

    fix settings

--- rakuten/rakuten/urls.py
+++ rakuten/rakuten/urls.py
@@ -17,10 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^login/', obtain_jwt_token),
     url(r'^api/',  include('animar.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
     # url(r'^mypage/$', AuthInfoGetView.as_view()),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
