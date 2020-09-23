bcb98d119a1686b27d1babdfd36e06ca27d59ac9
takurinton <takuirnton@gmail.com>
2020-09-23 11:13:16 +0900

    login

--- rakuten-frontend/src/components/Login.jsx
+++ rakuten-frontend/src/components/Login.jsx
@@ -1,38 +1,42 @@
 import React, {useState, useEffect} from 'react'
-
 import useLogin from '../hooks/useLogin'
-
 import { useHistory } from 'react-router-dom'
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import themeSignUp,{signup_style} from './theme'
+import RedditTextField from './RedditTextField'
+
+import LoginUI from './LoginUI'
+
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
 const Login = () => {
-
-    let history = useHistory()
-
-    const [user, setUser] = useState(null)
+    const classes = signup_style();
     const {
         handleChange, 
         handleSubmit, 
-        state, 
-        token, 
     } = useLogin()
-
-    const onChange = e => {
-        handleChange(e)
-    }
-    const onSubmit = e => {
-        e.preventDefault()
-        handleSubmit(state)
-        history.push('/main')
-    } 
-
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input type="text" name="user_id" value={state.user_id} onChange={onChange} placeholder="user_id" />
-                <input type="password" name="password" value={state.password} onChange={onChange} placeholder="password"/>
-                <input type="submit" />
-            </form>
-        </div>
+        <Container component="main" maxWidth="false"> 
+        <MuiThemeProvider theme={themeSignUp}>
+        <CssBaseline />
+            <LoginUI signup_style={signup_style} handleSubmit={handleSubmit} />
+        </MuiThemeProvider>
+        </Container>
     )
 }
 
---
+++ rakuten-frontend/src/components/LoginUI.jsx
@@ -0,0 +1,94 @@
+import React, {useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import RedditTextField from './RedditTextField'
+
+import { useHistory } from 'react-router-dom'
+import useLogin from '../hooks/useLogin'
+
+const LoginUI= ({signup_style, humanSubmit}) =>  {
+    const classes = signup_style();
+    let history = useHistory()
+    const {
+        handleChange, 
+        handleSubmit, 
+        state, 
+    } = useLogin()
+
+    const onChange = e => {
+        handleChange(e)
+    }
+    const onSubmit = e => {
+        e.preventDefault()
+        handleSubmit(state)
+    } 
+    
+    const [values, setValues] = useState({
+        showPassword: false,//ToDO I want to  togle show pass
+        showQestion:false, //To show "Are you human or Animal "
+        showAnimalSignUp:false, //To show " SignUp of Animal "
+    });
+
+    return(
+    <div className={classes.paper}>
+
+    <Typography   className={classes.animar} color="secondary" component="h1" variant="h4">
+    animar
+    </Typography>
+    <Typography className={classes.guide} color="secondary" component="h2" variant="h5">
+    Enter your
+    </Typography>
+    <Typography  color="secondary" component="h2" variant="h4">
+    email and password
+    </Typography>
+    
+    <form  onSubmit={onSubmit} className={classes.form,classes.sign_in_card} >
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+            <RedditTextField
+                variant="outlined"
+                required={true}
+                fullWidth
+                name="user_id"
+                label="user_id"
+                type="text"
+                id="user_id"
+                autoComplete="user_id"
+                value={state.user_id} 
+                onChange={onChange} 
+                placeholder="user_id"
+                validators={['user_id']}
+                            /> 
+            </Grid>
+            <Grid item xs={12}>
+            <RedditTextField
+                variant="outlined"
+                required={true}
+                fullWidth
+                id="standard-adornment-password"
+                type= 'password'
+                type={values.showPassword ? 'text' : 'password'}
+                label="password"
+                name="password"
+                // color="textPrimary"
+                // autoComplete="password"
+                value={state.password}
+                onChange={onChange}
+            />
+            </Grid>
+        </Grid>
+        <div>
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+        >
+            Login
+        </Button></div>
+
+    </form>
+    </div>) }
+export default LoginUI;
