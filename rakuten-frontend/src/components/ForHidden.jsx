f2fae8fde2d0fd563847231ad1dbf3a510502a3b
takurinton <takuirnton@gmail.com>
2020-09-23 12:52:18 +0900

    refactoring

--- rakuten-frontend/src/components/ForHidden.jsx
+++ rakuten-frontend/src/components/ForHidden.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import themeSignUp,{signup_style, error_page} from './theme'
+import themeSignUp,{ error_page } from './theme'
 import Typography from '@material-ui/core/Typography'
 
 const ForHidden = () => {
