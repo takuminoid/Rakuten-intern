62642fa22d37b9c681b9efb55300dcf2c9eadbbb
Takumi Katayama <50351271+takurinton@users.noreply.github.com>
2020-09-25 15:47:43 +0900

    Update getPostAPI.js

--- rakuten-frontend/src/api/getPostAPI.js
+++ rakuten-frontend/src/api/getPostAPI.js
@@ -1,22 +1,9 @@
+// author Takumi Katayama
+// author Kaito Imai
 import { useEffect, useState } from 'react'
 import toJson from '../toJson/toJson'
 
 var token = localStorage.getItem("token");
-
-const GetPosts = async ({page}) => {
-    const params  = {
-      searchText: '',
-      per_page: 15,
-      page:page,
-      key: '6657453-1ec2ff079372fc50cbf7f4d3b',
-      };
-    const qs = new URLSearchParams(params);
-    const resp = await fetch(
-      `https://pixabay.com/api?${qs}`
-    )
-    const data = await resp.json()
-    return await data
-  }
 
 const AllPost = async () => {
   const resp = await fetch(`http://localhost:8000/api/getpost/`, {
