c5673d68cce8d0d1cd610e074eb2314d57f004ea
Takumi Katayama <50351271+takurinton@users.noreply.github.com>
2020-09-25 16:00:59 +0900

    Update Userprof.jsx

--- rakuten-frontend/src/components/Userprof.jsx
+++ rakuten-frontend/src/components/Userprof.jsx
@@ -1,3 +1,5 @@
+// author Kaito Imai
+// author Shunsuke Hosomi
 import React, {useState, useEffect ,useRef, Fragment,useCallback} from 'react'
 import { Waypoint } from 'react-waypoint';
 import { makeStyles } from '@material-ui/core/styles';
